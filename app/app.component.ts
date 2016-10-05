6e1bdd752cb281f4369eb6e86c4f8d4fe09ea859
buyifly <buyifly@live.com>
2016-10-05 17:12:36 +1300

    Services and Dependency Injection

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from 'angular2/core';
+import { ProductListComponent } from './products/product-list.component';
+import { ProductService } from './products/product.service';
 
 @Component({
   selector: 'pm-app',
   template: `
     <div><h1>{{pageTitle}}</h1>
-      <div>My First Component</div>
+      <pm-products></pm-products>
     </div>
-  `
+  `,
+  directives: [ProductListComponent],
+  providers: [ProductService]
 })
 
 export class AppComponent {
